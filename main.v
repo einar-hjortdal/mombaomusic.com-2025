f40c3dd3b3b5f1cf14dfc8fcecb15938fdbd802d
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-12-19 17:33:44 +0100

    Fix typo

--- main.v
+++ main.v
@@ -12,6 +12,7 @@ const env_typekit_code = 'TYPEKIT_CODE'
 const env_bandsintown_api_key = 'BANDSINTOWN_API_KEY'
 const env_cache_duration = 'CACHE_DURATION'
 const env_debug = 'DEBUG'
+const static_directory = 'static'
 
 fn is_debug() bool {
 	return os.getenv(env_debug) == 'true'
@@ -92,7 +93,7 @@ fn get_bandsintown_client() &bandsintown.Client {
 	return bandsintown.new_client(bandsintown_api_key)
 }
 
-fn get_in_memoty_cache() &InMemoryCache {
+fn get_in_memory_cache() &InMemoryCache {
 	cache_duration_string := os.getenv(env_cache_duration)
 
 	if cache_duration_string == '' {
@@ -108,7 +109,6 @@ fn get_in_memoty_cache() &InMemoryCache {
 	}
 }
 
-// TODO on startup gzip statics that can be gzipped, middleware serve gzipped if browser supports it.
 fn main() {
 	dotenv.load()
 	set_log_level()
@@ -116,10 +116,10 @@ fn main() {
 	mut app := &App{
 		typekit_code:       get_typekit_code()
 		bandsintown_client: get_bandsintown_client()
-		cache:              get_in_memoty_cache()
+		cache:              get_in_memory_cache()
 	}
 
-	app.handle_static('static', false)!
+	app.handle_static(static_directory, false)!
 
 	veb.run[App, Context](mut app, 8080)
 }
